cae040ccf9137621f638d839bab12f7eaa7b2555
KeNiHx <kennyhuylam@gmail.com>
2021-04-30 12:34:54 -0600

    added methods for dependency injection with spring annotation

---
+++ src/com/klam/springdemo/FortuneService.java
@@ -0,0 +1,6 @@
+package com.klam.springdemo;
+
+public interface FortuneService {
+	
+	public String getFortune();
+}
--- src/com/klam/springdemo/SoccerCoach.java
+++ src/com/klam/springdemo/SoccerCoach.java
@@ -1,13 +1,27 @@
 package com.klam.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SoccerCoach implements Coach {
 	
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public SoccerCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your free kicks";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fortuneService.getFortune();
 	}
 
 }
