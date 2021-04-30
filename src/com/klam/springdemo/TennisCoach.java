8143e27e8cb0c6b873818b14b277888924ef0f19
KeNiHx <kennyhuylam@gmail.com>
2021-04-30 14:00:31 -0600

    added init and destroy methods into TennisCoach to demonstrate @PostConstruct and @PreDestroy

--- src/com/klam/springdemo/TennisCoach.java
+++ src/com/klam/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.klam.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	@Autowired
@@ -17,6 +19,18 @@ public class TennisCoach implements Coach {
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	
+	}
+	
+	//define init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff");
+	}
+	
+	//define destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff");
 	}
 	
 	@Override
